6194d151b00c08eaab802320cf916b7fc7a1b6d3
Kate2mi <105608198+Kate2mi@users.noreply.github.com>
2022-07-11 18:41:16 -0700

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,12 +4,13 @@ def call(String stageName){
      {
        sh "mvn clean package"
      } 
+        else if ("${stageName}" == "Upload Into Nexus")
+     {
+       sh "mvn clean deploy"
+     }
         else if ("${stageName}" == "SonarQube Report")
      {
        sh "mvn clean sonar:sonar"
      }
-       else if ("${stageName}" == "Upload Into Nexus")
-     {
-       sh "mvn deploy"
-     }
+      
 }
